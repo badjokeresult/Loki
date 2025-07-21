f92d878c9624207f051f31a5ef932694342b5b77
badjokeresult <badjokeresult@gmail.com>
2025-07-21 12:22:11 +0300

    determine cwd

--- launcher.py
+++ launcher.py
@@ -1,8 +1,13 @@
+import os
 import loki
 import upgrader
 
 
 if __name__ == "__main__":
+    working_dir = "C:\\Loki"
+    os.makedirs(working_dir, exist_ok=True)
+    os.chdir(working_dir)
+    
     upgrader_args = upgrader.main()
     upgrader_args.sigsonly = True
     upgrader.do_upgrade(upgrader_args)
